cddc669745809728fd271c466f0bc897066a90d7
TWS <CuinaRPGMaker@googlemail.com>
2014-01-06 23:06:36 +0100

    untracked files on master: 3e4285e Plugin widget und movement erstellt. Anzeigebug in Map-Editor in der Toolbar behoben.

---
+++ engine/plug/src/cuina/morpg/MOGame.java
@@ -0,0 +1,80 @@
+package cuina.morpg;
+
+import cuina.network.server.ServerEvent;
+import cuina.network.server.ServerListener;
+
+public class MOGame
+{
+	class ServerHandler implements ServerListener
+	{
+		@Override
+		public void clientConnected(ServerEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void clientDisconnected(ServerEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void sessionCreated(ServerEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void sessionDestroyed(ServerEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void sessionJoined(ServerEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void sessionLeaved(ServerEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void roomCreated(ServerEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void roomDestroyed(ServerEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void roomJoined(ServerEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void roomLeaved(ServerEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+	}
+}
